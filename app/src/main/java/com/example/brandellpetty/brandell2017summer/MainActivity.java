8390aa38a05c20e0f87e87420729df35ae781d6b
brandellpetty <brandell_j_petty@hotmail.com>
2017-06-07 11:04:28 -0400

    Updated after class.

--- app/src/main/java/com/example/brandellpetty/brandell2017summer/MainActivity.java
+++ app/src/main/java/com/example/brandellpetty/brandell2017summer/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.brandellpetty.brandell2017summer;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.brandellpetty.brandell2017summer.fragment.DemoFragment;
+import com.example.brandellpetty.brandell2017summer.fragment.WorkFragment;
 
 import org.w3c.dom.Text;
 
@@ -24,20 +28,35 @@ public class MainActivity extends AppCompatActivity {
     private void initialView() {
         tv_demo = (TextView)findViewById(R.id.activity_tool_demo);
         tv_work = (TextView)findViewById(R.id.activity_tool_work);
+        WorkFragment workFragment = new WorkFragment();
+        DemoFragment demoFragment = new DemoFragment();
     }
 
     private void setListener(){
+       final WorkFragment workFragment = new WorkFragment();
+        final DemoFragment demoFragment = new DemoFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.activity_main_fragment,demoFragment).commit();
+
         tv_demo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                Toast.makeText(MainActivity.this, "You clicked demo", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, "You clicked demo", Toast.LENGTH_SHORT).show();
+                tv_work.setTextColor(Color.BLACK);
+                tv_demo.setTextColor(Color.RED);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.activity_main_fragment,demoFragment).commit();
             }
         });
 
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "You clicked work", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(MainActivity.this, "You clicked work", Toast.LENGTH_SHORT).show();
+                tv_work.setTextColor(Color.RED);
+                tv_demo.setTextColor(Color.BLACK);
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.activity_main_fragment,workFragment).commit();
             }
         };
         tv_work.setOnClickListener(listener);
