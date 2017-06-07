e615fcdba6987dd18d25631147efc1e253ec9ff6
brandellpetty <brandell_j_petty@hotmail.com>
2017-06-07 09:10:01 -0400

    Updated after class.

--- app/src/main/java/com/example/brandellpetty/brandell2017summer/MainActivity.java
+++ app/src/main/java/com/example/brandellpetty/brandell2017summer/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.brandellpetty.brandell2017summer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_demo;
+    private TextView tv_work;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialView();
+        setListener();
+    }
+
+    private void initialView() {
+        tv_demo = (TextView)findViewById(R.id.activity_tool_demo);
+        tv_work = (TextView)findViewById(R.id.activity_tool_work);
+    }
+
+    private void setListener(){
+        tv_demo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "You clicked demo", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked work", Toast.LENGTH_SHORT).show();
+            }
+        };
+        tv_work.setOnClickListener(listener);
+    }
+
+    public void login(View v){
+        Toast.makeText(MainActivity.this, "You clicked login", Toast.LENGTH_SHORT).show();
     }
 }
